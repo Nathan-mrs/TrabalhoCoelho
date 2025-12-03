763a8c8daa17e7957d742f908d47ff1acc44075a
Nathan <nathanmalmeida2007@gmail.com>
2025-12-03 15:59:28 -0300

    criei as class dentro do models dentro de frequencia

--- frequencia/models.py
+++ frequencia/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class aluno(models.Model):
+    nome = models.CharField(max_length=100)
+    matricula = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.nome
+    
+class frequencia(models.Model):
+    aluno = models.ForeignKey(aluno, on_delete=models.CASCADE)
+    data = models.DateField()
+    presente = models.BooleanField(default=False)
+
+    def __str__(self):
+        status = "Presente" if self.presente else "Ausente"
+        return f"{self.aluno.nome} - {self.data} - {status}"
