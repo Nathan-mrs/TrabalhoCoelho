97ed5c1a4cdedebaa9d3ad80f1a980374be47490
Nathan <nathanmalmeida2007@gmail.com>
2025-12-03 16:35:42 -0300

    salvando alterações

--- frequencia/models.py
+++ frequencia/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class aluno(models.Model):
     nome = models.CharField(max_length=100)
     matricula = models.CharField(max_length=20, unique=True)
+    curso = models.CharField(max_length=100)
 
     def __str__(self):
         return self.nome
@@ -11,6 +12,7 @@ class frequencia(models.Model):
     aluno = models.ForeignKey(aluno, on_delete=models.CASCADE)
     data = models.DateField()
     presente = models.BooleanField(default=False)
+    
 
     def __str__(self):
         status = "Presente" if self.presente else "Ausente"
