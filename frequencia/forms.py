1bef1404b4f549ac06309b59706e04cdd4654b3c
Nathan <nathanmalmeida2007@gmail.com>
2025-12-09 00:45:01 -0300

    Fiz todo o style do sistema, e corrigi alguns erros

--- frequencia/forms.py
+++ frequencia/forms.py
@@ -7,6 +7,18 @@ class AlunoForm(forms.ModelForm):
         fields = ['nome', 'matricula', 'curso']
 
 class FrequenciaForm(forms.ModelForm):
+
+    data = forms.DateField(
+        widget=forms.DateInput(
+            attrs={
+                "type": "date",
+                "placeholder": "DD/MM/AAAA",  # pode aparecer dependendo do navegador
+                "class": "campo-data",
+            }
+        )
+    )
+
     class Meta:
         model = frequencia
         fields = ['aluno', 'data', 'presente']
+
